99d1323972f96100eaa0265d8a9755f4881e208f
Artem Klimov <jakmobius@gmail.com>
2020-09-07 19:24:43 +0300

    Update square_equations.c

--- square_equations.c
+++ square_equations.c
@@ -1,49 +1,121 @@
 #include <stdio.h>
+#include <math.h>
 
-typedef char byte;
+#define CALL_TEST(method, args...) method(__LINE__, args)
 
-void swapbytes(byte* p1, byte* p2, size_t size) {
-    for(size_t p = 0; p < size; p++) {
-        byte buffer = *(p1 + p);
-        *(p1 + p) = *(p2 + p);
-        *(p2 + p) = buffer;
+enum RootCount {
+    none, one, two, infinity, error
+};
+
+typedef enum RootCount RootCount;
+
+const char* oneRoot = "one root";
+const char* twoRoots = "two roots";
+const char* noneRoots = "zero roots";
+const char* infiniteRoots = "infinite roots";
+const char* solverError = "solver error";
+
+RootCount solveEquation(double a, double b, double c, double roots[2]) {
+    
+    if(isnan(a) || isnan(b) || isnan(c) || roots == NULL) {
+        return error;
+    }
+    
+    if(a == 0 && b == 0 && c == 0) return infinity;
+    
+    double discriminant = b * b - 4 * a * c;
+    
+    if(discriminant == 0) {
+        roots[0] = -(b / (a * 2));
+        return one;
+    }
+    
+    if(discriminant < 0) {
+        return none;
+    }
+    
+    discriminant = sqrt(discriminant);
+    
+    roots[0] = (-b + discriminant) / (a * 2);
+    roots[1] = (-b - discriminant) / (a * 2);
+    
+    return two;
+}
+
+const char* responseToStr(RootCount response) {
+    if(response == one) return oneRoot;
+    if(response == two) return twoRoots;
+    if(response == none) return noneRoots;
+    if(response == infinity) return infiniteRoots;
+    return solverError;
+}
+
+short testResponse(RootCount response, RootCount expected, int line) {
+    if(response == expected) return 1;
+    printf("Test failed on line %d: %s expected, got %s\n", line, responseToStr(expected), responseToStr(response));
+    return 0;
+}
+
+
+void testSolverOneRoot(int line, double a, double b, double c, double root) {
+    double roots[2];
+    RootCount result = solveEquation(a, b, c, roots);
+    if(testResponse(result, one, line)) {
+        if(root != roots[0])
+            printf("Test failed on line %d: single %lf root expected, got %lf\n", line, root, roots[0]);
     }
 }
 
-void sort(void* data, size_t n, size_t size, int (*comparator)(void* a, void* b)) {
-    
-    byte* bytes = (byte*) data;
-    byte* p1;
-    
-    for(size_t i = 0; i < n; i++) {
-        p1 = bytes + size * i;
-        for(size_t j = 0; j < n; j++) {
-            byte* p2 = bytes + size * j;
-            if(comparator(p1, p2) > 0) {
-                swapbytes(p1, p2, size);
-            }
-        }
+void testSolverTwoRoots(int line, double a, double b, double c, double root1, double root2) {
+    double roots[2];
+    RootCount result = solveEquation(a, b, c, roots);
+    if(testResponse(result, two, line)) {
+        if(root1 != roots[0] || root2 != roots[1])
+            printf("Test failed on line %d: expected roots %lf, %lf, got %lf, %lf\n", line, root1, root2, roots[0], roots[1]);
     }
 }
 
-int comparator(void* a, void* b) {
-    return *(int*)b - *(int*)a;
+void testSolverResponse(int line, double a, double b, double c, RootCount response) {
+    double roots[2];
+    RootCount result = solveEquation(a, b, c, roots);
+    testResponse(result, response, line);
+}
+
+void test() {
+    CALL_TEST(testSolverOneRoot, 1, 2, 1, -1);
+    CALL_TEST(testSolverOneRoot, 1, -2, 1, 1);
+    CALL_TEST(testSolverTwoRoots, -2, -2, 4, -2, 1);
+    CALL_TEST(testSolverTwoRoots, 4, 2, -2, 0.5, -1);
+    CALL_TEST(testSolverResponse, 0, 0, 0, infinity);
+    CALL_TEST(testSolverResponse, 5, 5, 5, none);
+    CALL_TEST(testSolverResponse, 1, 1, 1, none);
+}
+
+void solveUserEquation() {
+    double a, b, c;
+    double result[2];
+    
+    printf("Enter a, b, c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    
+    RootCount rootCount = solveEquation(a, b, c, result);
+    
+    if(rootCount == none) {
+        printf("no roots\n");
+    } else if(rootCount == one) {
+        printf("root: %lf\n", result[0]);
+    } else if(rootCount == two) {
+        printf("roots: %lf, %lf\n", result[0], result[1]);
+    } else if(rootCount == infinity) {
+        printf("infinite roots\n");
+    }
 }
 
 int main(int argc, const char * argv[]) {
     
-    int intarray[] = {1, 4, 3, 7, 5, 4, 3};
+    test();
     
-    size_t length = sizeof(intarray) / sizeof(intarray[0]);
-    
-    sort(intarray, length, sizeof(intarray[0]), &comparator);
-    
-    for(int i = 0; i < length; i++) {
-        printf("%d ", intarray[i]);
-    }
-    
-    printf("\n");
+    for(;;) solveUserEquation();
     
     return 0;
 }
-
